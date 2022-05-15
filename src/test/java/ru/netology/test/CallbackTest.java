38a6cd34660ebcc5bd7f4f8fb625196688955e51
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-16 01:35:38 +0300

    base test on form

--- src/test/java/ru/netology/test/CallbackTest.java
+++ src/test/java/ru/netology/test/CallbackTest.java
@@ -4,27 +4,44 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
-    public void setUp() {
+    static void setUp() {
         System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
 
     @BeforeEach
-    void setUp2() {
+    public void setUp2() {
         driver = new ChromeDriver();
     }
 
     @AfterEach
-    public void close() {
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
+    @Test
+    public void shouldTest(){
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Андрей");
+        elements.get(1).sendKeys("+79843333344");
+
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
 }
