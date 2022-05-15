7c75477db3132dda5bdb39e69d99b7fcad528be7
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-16 00:43:29 +0300

    driver

--- src/test/java/ru/netology/test/CallbackTest.java
+++ src/test/java/ru/netology/test/CallbackTest.java
@@ -1,8 +1,30 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CallbackTest {
-    @Test
+    private WebDriver driver;
+
+    @BeforeAll
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp2() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void close() {
+        driver.quit();
+        driver = null;
+    }
 
 }
