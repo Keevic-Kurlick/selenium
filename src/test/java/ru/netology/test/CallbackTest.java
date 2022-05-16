8ee23549cdc70726362bba6a9efd1c5602559909
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-16 03:03:14 +0300

    added tests

--- src/test/java/ru/netology/test/CallbackTest.java
+++ src/test/java/ru/netology/test/CallbackTest.java
@@ -1,15 +1,15 @@
 package ru.netology.test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,10 +19,16 @@ public class CallbackTest {
     @BeforeAll
     static void setUp() {
         System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     public void setUp2() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver = new ChromeDriver();
     }
 
@@ -33,14 +39,42 @@ public class CallbackTest {
     }
 
     @Test
-    public void shouldTest(){
+    public void shouldTestPositiveCase() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
-        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79998883344");
-
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79998883344");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.tagName("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    public void shouldTestValidationEmptyName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79998883344");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.tagName("button")).click();
+        String sub = driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", sub);
+    }
+
+    @Test
+    public void shouldTestValidationEmptyPhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.tagName("button")).click();
+        String sub = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", sub);
+    }
+
+    @Test
+    public void shouldTestValidationEmptyAll() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.tagName("button")).click();
+        String sub = driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", sub);
     }
 }
