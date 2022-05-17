66c6fc68d42e29888278ea5655222f5689a0683f
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-17 16:06:39 +0300

    not clicking on checkbox test

--- src/test/java/ru/netology/test/CallbackTest.java
+++ src/test/java/ru/netology/test/CallbackTest.java
@@ -41,7 +41,7 @@ public class CallbackTest {
     public void shouldTestPositiveCase() {
         driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Андрей");
         driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79998883344");
-        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+    //    driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.tagName("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
