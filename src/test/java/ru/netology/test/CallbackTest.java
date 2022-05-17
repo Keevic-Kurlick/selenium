426faddea9fff95e3904460bc666b3d5febce129
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-17 19:06:58 +0300

    added new tests on validation

--- src/test/java/ru/netology/test/CallbackTest.java
+++ src/test/java/ru/netology/test/CallbackTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CallbackTest {
     private WebDriver driver;
@@ -49,7 +50,6 @@ public class CallbackTest {
 
     @Test
     public void shouldTestValidationEmptyName() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79998883344");
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.tagName("button")).click();
@@ -59,20 +59,44 @@ public class CallbackTest {
 
     @Test
     public void shouldTestValidationEmptyPhone() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Андрей");
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.tagName("button")).click();
         String sub = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__sub")).getText();
         assertEquals("Поле обязательно для заполнения", sub);
     }
+    @Test
+    public void shouldTestValidationInvalidName() {
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("John");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79998883344");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.tagName("button")).click();
+        String sub = driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", sub);
+    }
+
+    @Test
+    public void shouldTestValidationInvalidPhone() {
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("telefon");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.tagName("button")).click();
+        String sub = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", sub);
+    }
 
     @Test
     public void shouldTestValidationEmptyAll() {
-        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
         driver.findElement(By.tagName("button")).click();
         String sub = driver.findElement(By.cssSelector("[data-test-id=\"name\"] .input__sub")).getText();
         assertEquals("Поле обязательно для заполнения", sub);
+    }
+    @Test
+    public void shouldTestValidationEmptyCheckbox() {
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79012345678");
+        driver.findElement(By.tagName("button")).click();
+        assertNotNull(driver.findElement(By.className("input_invalid")));
     }
 }
